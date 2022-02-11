15f48ded4bb2420e72a41e08c18dbfd749b23833
Sarah Marshall <marshallsaraha@gmail.com>
2022-02-11 15:56:01 -0800

    Add gravity

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(RapierConfiguration {
+            gravity: Vector::y() * -9.81,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_startup_system(setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("sprites/cat_alive.png"),
-        ..Default::default()
-    });
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("sprites/cat_alive.png"),
+            ..Default::default()
+        })
+        .insert_bundle(RigidBodyBundle::default())
+        .insert_bundle(ColliderBundle::default())
+        .insert(RigidBodyPositionSync::Discrete);
 }
