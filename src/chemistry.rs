4e648418f662c36500d8ada7934635292e1678de
Sarah Marshall <marshallsaraha@gmail.com>
2022-02-12 15:28:44 -0800

    Add rule types

--- src/chemistry.rs
+++ src/chemistry.rs
@@ -1,3 +1,4 @@
+use self::Property::*;
 use bevy::prelude::*;
 use std::collections::HashMap;
 
@@ -27,7 +28,19 @@ struct Chemistry {
     properties: HashMap<Property, f32>,
 }
 
-type Selector = Box<dyn Fn(Entity) -> HashMap<Entity, f32>>;
+type Selector = Box<dyn Fn(World, Entity) -> HashMap<Entity, f32> + Sync>;
+
+impl From<Property> for Selector {
+    fn from(property: Property) -> Self {
+        Box::new(move |world, entity| {
+            let chemistry = world.entity(entity).get::<Chemistry>().unwrap();
+            match chemistry.properties.get(&property) {
+                None => HashMap::new(),
+                Some(&value) => HashMap::from([(entity, value)]),
+            }
+        })
+    }
+}
 
 enum UnaryOperator {
     Produce,
@@ -45,6 +58,12 @@ struct ScaledProperty {
     property: Property,
 }
 
+impl ScaledProperty {
+    fn new(strength: f32, property: Property) -> ScaledProperty {
+        ScaledProperty { strength, property }
+    }
+}
+
 enum Effect {
     Unary(UnaryOperator, ScaledProperty),
     Binary(ScaledProperty, BinaryOperator, ScaledProperty),
@@ -56,40 +75,82 @@ struct Rule {
     effects: Vec<Effect>,
 }
 
+impl Rule {
+    fn select2(mut self, selector1: impl Into<Selector>, selector2: impl Into<Selector>) -> Rule {
+        self.selectors.push(selector1.into());
+        self.selectors.push(selector2.into());
+        self
+    }
+
+    fn produce(mut self, strength: f32, property: Property) -> Rule {
+        self.effects.push(Effect::Unary(
+            UnaryOperator::Produce,
+            ScaledProperty::new(strength, property),
+        ));
+        self
+    }
+
+    fn consume(mut self, strength: f32, property: Property) -> Rule {
+        self.effects.push(Effect::Unary(
+            UnaryOperator::Consume,
+            ScaledProperty::new(strength, property),
+        ));
+        self
+    }
+
+    fn share(mut self, strength: f32, property: Property) -> Rule {
+        self.effects.push(Effect::Unary(
+            UnaryOperator::Share,
+            ScaledProperty::new(strength, property),
+        ));
+        self
+    }
+}
+
 const EPSILON: f32 = 1e-6;
 
-fn select_property(world: World, property: Property) -> Selector {
-    Box::new(move |entity| {
-        let chemistry = world.entity(entity).get::<Chemistry>().unwrap();
-        match chemistry.properties.get(&property) {
-            None => HashMap::new(),
-            Some(&value) => HashMap::from([(entity, value)]),
-        }
-    })
+lazy_static! {
+    static ref NATURAL_RULES: Vec<Rule> = vec![
+        rule(1.0)
+            .select2(any(Burning), Flammable)
+            .produce(1.0, Burning),
+        rule(0.1).select2(Burning, area(1.0)).share(1.0, Burning),
+        rule(0.1).consume(1.0, Burning),
+    ];
 }
 
-fn select_area(_world: World, _radius: f32) -> Selector {
+fn area(_radius: f32) -> Selector {
     todo!()
 }
 
-fn select_sight(_world: World) -> Selector {
+fn sight() -> Selector {
     todo!()
 }
 
-fn any(selector: Selector) -> Selector {
-    Box::new(move |entity| {
-        selector(entity)
+fn any(selector: impl Into<Selector>) -> Selector {
+    let selector = selector.into();
+    Box::new(move |world, entity| {
+        selector(world, entity)
             .iter()
             .map(|(&entity, &value)| (entity, if value > EPSILON { 1.0 } else { 0.0 }))
             .collect()
     })
 }
 
-fn not(selector: Selector) -> Selector {
-    Box::new(move |entity| {
-        selector(entity)
+fn not(selector: impl Into<Selector>) -> Selector {
+    let selector = selector.into();
+    Box::new(move |world, entity| {
+        selector(world, entity)
             .iter()
             .map(|(&entity, &value)| (entity, 1.0 - value))
             .collect()
     })
+}
+
+fn rule(strength: f32) -> Rule {
+    Rule {
+        strength,
+        selectors: vec![],
+        effects: vec![],
+    }
 }
